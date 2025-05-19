f786fe98ba0d5f08a6e18d5736eff2ab6a14839f
Ilton Pfleger Júnior <pfleger@ssh.lisha.ufsc.br>
2025-05-19 19:35:57 -0300

    add bluetooth applet

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 if ! grep -Fq "____PJ-SETUP____"  "${HOME}/.bashrc"; then
 	echo '#____PJ-SETUP____' >> $HOME/.bashrc
 	echo 'source ${HOME}/.dotfiles/bashrc' >> $HOME/.bashrc
-	sudo pacman -S dmenu xorg-server xorg-xinit libxft ttf-fira-mono ttf-font-awesome xorg-xrandr feh network-manager-applet
+	sudo pacman -S dmenu xorg-server xorg-xinit libxft ttf-fira-mono ttf-font-awesome xorg-xrandr feh
 	sudo pacman -S pipewire pipewire-alsa pipewire-pulse wireplumber rtkit pavucontrol
+	sudo pacman -S blueman network-manager-applet maim
 	sudo usermod -aG audio $USER
 	sudo usermod -aG video $USER
 	ln -s $HOME/.dotfiles/startx $HOME/.xinitrc
