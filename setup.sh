b3004cfc0ec910159b9405322f692521d4086743
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-05-19 07:12:40 -0300

    update

--- setup.sh
+++ setup.sh
@@ -3,10 +3,11 @@
 if ! grep -Fq "____PJ-SETUP____"  "${HOME}/.bashrc"; then
 	echo '#____PJ-SETUP____' >> $HOME/.bashrc
 	echo 'source ${HOME}/.dotfiles/bashrc' >> $HOME/.bashrc
-	sudo pacman -S xorg-server xorg-xinit libxft ttf-fira-mono rofi ttf-font-awesome ttf-cascadia-code feh
+	sudo pacman -S xorg-server xorg-xinit libxft ttf-fira-mono rofi ttf-font-awesome xorg-xrandr feh
 	sudo pacman -S pipewire pipewire-alsa pipewire-pulse wireplumber rtkit pavucontrol
 	sudo usermod -aG audio $USER
 	sudo usermod -aG video $USER
+	ln -s $HOME/.dotfiles/startx $HOME/.xinitrc
 	#ssh-keygen -t rsa -b 4096 -C "pfleger@lisha.ufsc.br"
 	#ssh-copy-id pfleger@ssh.lisha.ufsc.br
 	#ssh-copy-id  -o ProxyJump=pfleger@ssh.lisha.ufsc.br pj@150.162.62.155
