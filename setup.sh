81557cb43a72cf4b60e0c3dba527fce5d778c057
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-07-24 08:10:02 -0300

    updates

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sudo pacman -S gnome-session gnome-shell gdm gnome-control-center nautilus ttf-fira-code firefox wl-clipboard
 ln -s $HOME/.dotfiles/bashrc $HOME/.bashrc
 dconf load /org/gnome/ < $HOME/.dotfiles/gnome/gnome.txt
