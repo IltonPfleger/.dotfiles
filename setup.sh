127c79a75e90afefb3fde98b360afd519d0255df
Ilton Pfleger Júnior <ilton.pfleger@grad.ufsc.br>
2026-01-05 18:10:05 -0300

    sync

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@ mv $HOME/.bashrc $HOME/bashrc.back
 ln -s $HOME/.dotfiles/bashrc $HOME/.bashrc
 ln -s $HOME/.dotfiles/clang-format $HOME/.clang-format
 $HOME/.dotfiles/gnome/load.sh
+
+
+#Galaxy Book 3 
+echo 80 | sudo tee /sys/class/power_supply/BAT1/charge_control_end_threshold
+echo true | sudo tee /sys/class/firmware-attributes/samsung-galaxybook/attributes/power_on_lid_open/current_value
+
