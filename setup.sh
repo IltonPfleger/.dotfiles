2c18c0e1bb2442f1eeb361ef94706d054ca8070c
Ilton Pfleger Júnioe <pfleger@lisha.ufsc.br>
2026-03-13 21:29:18 -0300

    sync

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ mv $HOME/.bashrc $HOME/bashrc.back
 ln -s $HOME/.dotfiles/vim $HOME/.vim
 ln -s $HOME/.dotfiles/bashrc $HOME/.bashrc
 $HOME/.dotfiles/gnome/load.sh
+git submodule update --init --recursive
